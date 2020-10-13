d4e233655f2cc0b9a3c248406943733a70c842bf
Y-KUN-21 <57254049+Y-KUN-21@users.noreply.github.com>
2020-10-13 19:16:30 +0530

    Add files via upload

---
+++ lib/myHome.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+class MyHome extends StatefulWidget {
+  @override
+  _MyHomeState createState() => _MyHomeState();
+}
+
+class _MyHomeState extends State<MyHome> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        shrinkWrap: true,
+        scrollDirection: Axis.vertical,
+        itemCount: 15,
+        itemBuilder: (BuildContext context, int index) {
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.transparent,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              width: double.infinity,
+              height: 350,
+              child: Card(
+                elevation: 1.0,
+                borderOnForeground: true,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ClipRRect(
+                      child: Image.network(
+                        "https://cdn.pixabay.com/photo/2019/10/23/06/53/china-4570626_960_720.jpg",
+                        width: double.infinity,
+                        height: 250,
+                        fit: BoxFit.fill,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(10.0, 10, 10, 20),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "2020.14.oct".toUpperCase(),
+                                style:
+                                    TextStyle(fontSize: 15, color: Colors.grey),
+                              ),
+                              Text(
+                                "A1 wood works",
+                                style: TextStyle(
+                                    fontSize: 25,
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.w400),
+                              )
+                            ],
+                          ),
+                          Container(
+                            height: 50,
+                            decoration: BoxDecoration(
+                                color: Colors.blue, shape: BoxShape.circle),
+                            child: Image.network(
+                                "https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/batman_hero_avatar_comics-512.png"),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          );
+        });
+  }
+}
